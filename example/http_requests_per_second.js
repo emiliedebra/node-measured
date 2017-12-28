d4b9693b65c57ad2616193234861b0a3541d2d27
Emilie Wood <emilie@anotherway.co.za>
2017-12-28 07:13:52 +0200

    add Flow and get example to work

--- example/http_requests_per_second.js
+++ example/http_requests_per_second.js
@@ -1,16 +1,19 @@
-'use strict';
+/* @flow */
 
-var metrics    = require('..');
-var collection = new metrics.Collection('http');
-var http       = require('http');
+// 'use strict';
+const metrics = require('..');
 
-var rps = collection.meter('requestsPerSecond');
-http.createServer(function (req, res) {
+const collection = new metrics.Collection('http');
+
+const http = require('http');
+
+const rps = collection.meter('requestsPerSecond');
+http.createServer((req, res) => {
   console.error(req.headers['content-length']);
   rps.mark();
   res.end('Thanks');
 }).listen(8000);
 
-setInterval(function () {
+setInterval(() => {
   console.log(collection.toJSON());
 }, 1000);
